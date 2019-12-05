9bbcb584fc87a41cb271bc2b33ccf44d96db210c
Jaman Dedy <jeandedieuam@gmail.com>
2019-12-05 14:38:19 +0200

    lazy routing suspense

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Suspense } from "react";
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import User from "./containers/User";
+import Welcome from "./containers/Welcome";
+
+const Posts = React.lazy(() => import("./containers/Posts"));
+class App extends Component {
+  state = { show: false };
+
+  modeHandler = () => {
+    this.setState(prevState => {
+      return { show: !prevState.show };
+    });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <button onClick={this.modeHandler}>Toggle Mode</button>
+        {this.state.show ? (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Posts />
+          </Suspense>
+        ) : (
+          <User />
+        )}
+      </React.Fragment>
+      // <BrowserRouter>
+      //   <React.Fragment>
+      //     <nav>
+      //       <NavLink to="/user">User Page</NavLink> |&nbsp;
+      //       <NavLink to="/posts">Posts Page</NavLink>
+      //     </nav>
+      //     <Route path="/" component={Welcome} exact />
+      //     <Route path="/user" component={User} />
+      //     <Route
+      //       path="/posts"
+      //       render={() => (
+      //         <Suspense fallback={<div>Loading...</div>}>
+      //           <Posts />
+      //         </Suspense>
+      //       )}
+      //     />
+      //   </React.Fragment>
+      // </BrowserRouter>
+    );
+  }
 }
 
 export default App;
